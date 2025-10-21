c1aa1491723e5380d61103ca7b2093d058c75e47
bordavictoria <bordavictoria@hotmail.com>
2025-10-21 00:19:45 -0300

    Hay que charlar si queremos usar bootstraap

--- Tp_base/FRONTEND/js/api.js
+++ Tp_base/FRONTEND/js/api.js
@@ -1,15 +1,15 @@
 export const productos = [ 
-  { id: 1, nombre: "Anana", precio: 120, rutaImg: "assets/img/anana.jpg", categoria: "Otras frutas", activo: true },
-  { id: 2, nombre: "Arandano", precio: 90, rutaImg: "assets/img/arandano.jpg", categoria: "Otras frutas", activo: true },
-  { id: 3, nombre: "Naranja", precio: 110, rutaImg: "assets/img/naranja.jpg", categoria: "Cítricos", activo: true },
-  { id: 4, nombre: "Pera", precio: 130, rutaImg: "assets/img/pera.jpg", categoria: "Otras frutas", activo: true },
-  { id: 5, nombre: "Banana", precio: 150, rutaImg: "assets/img/banana.jpg", categoria: "Otras frutas", activo: true },
-  { id: 6, nombre: "Manzana", precio: 200, rutaImg: "assets/img/manzana.jpg", categoria: "Otras frutas", activo: true },
-  { id: 7, nombre: "Mandarina", precio: 300, rutaImg: "assets/img/mandarina.jpg", categoria: "Cítricos", activo: true },
-  { id: 8, nombre: "Frambuesa", precio: 280, rutaImg: "assets/img/frambuesa.png", categoria: "Otras frutas", activo: true },
-  { id: 9, nombre: "Frutilla", precio: 220, rutaImg: "assets/img/frutilla.jpg", categoria: "Otras frutas", activo: true },
-  { id: 10, nombre: "Kiwi", precio: 180, rutaImg: "assets/img/kiwi.jpg", categoria: "Otras frutas", activo: true },
-  { id: 11, nombre: "Pomelo-amarillo", precio: 250, rutaImg: "assets/img/pomelo-amarillo.jpg", categoria: "Cítricos", activo: true },
-  { id: 12, nombre: "Pomelo-rojo", precio: 270, rutaImg: "assets/img/pomelo-rojo.jpg", categoria: "Cítricos", activo: true },
-  { id: 13, nombre: "Sandia", precio: 240, rutaImg: "assets/img/sandia.jpg", categoria: "Otras frutas", activo: true }
+  { id: 1, nombre: "guitarra1", precio: 120, rutaImg: "assets/img/guitarra1.jpg", categoria: "Guitarra", activo: true },
+  { id: 2, nombre: "guitarra2", precio: 90, rutaImg: "assets/img/guitarra2.jpg", categoria: "Guitarra", activo: true },
+  { id: 3, nombre: "guitarra3", precio: 110, rutaImg: "assets/img/guitarra3.jpg", categoria: "Guitarra", activo: true },
+  { id: 4, nombre: "guitarra4", precio: 130, rutaImg: "assets/img/guitarra4.jpg", categoria: "Guitarra", activo: true },
+  { id: 5, nombre: "guitarra5", precio: 150, rutaImg: "assets/img/guitarra5.jpg", categoria: "Guitarra", activo: true },
+  { id: 6, nombre: "piano1", precio: 200, rutaImg: "assets/img/piano1.jpg", categoria: "Piano", activo: true },
+  { id: 7, nombre: "piano2", precio: 300, rutaImg: "assets/img/piano2.jpg", categoria: "Piano", activo: true },
+  { id: 8, nombre: "piano3", precio: 280, rutaImg: "assets/img/piano3.jpg", categoria: "Piano", activo: true },
+  { id: 9, nombre: "piano4", precio: 220, rutaImg: "assets/img/piano4.jpg", categoria: "Piano", activo: true },
+  { id: 10, nombre: "piano5", precio: 180, rutaImg: "assets/img/piano5.jpg", categoria: "Piano", activo: true },
+  { id: 11, nombre: "piano6", precio: 250, rutaImg: "assets/img/piano6.jpg", categoria: "Piano", activo: true },
+  { id: 12, nombre: "piano7", precio: 270, rutaImg: "assets/img/piano7.jpg", categoria: "Piano", activo: true },
+  { id: 13, nombre: "piano8", precio: 240, rutaImg: "assets/img/piano8.jpg", categoria: "Piano", activo: true }
 ];
